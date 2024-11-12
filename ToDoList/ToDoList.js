fdf1289cb0e95f2ac95487771216e112eeee5022
Cristian Dragan <cristiandragan34@yahoo.ro>
2024-11-12 19:51:46 +0200

    Reseted Commit

--- ToDoList/ToDoList.js
+++ ToDoList/ToDoList.js
@@ -2,16 +2,6 @@ import { createStackNavigator } from "@react-navigation/native/src/__stubs__/cre
 import ToDoListStack from "../src/components/ToDoListStack";
 import * as React from "react";
 
-const Stack = createStackNavigator();
-
 export default function ToDoList({ toDo, navigation }) {
-  return (
-    // <Stack.Navigator>
-    //   <Stack.Screen name={"List"}>
-    //     {(props) => <ToDoListStack {...props} toDo={toDo} />}
-    //   </Stack.Screen>
-    //   <Stack.Screen name={"TaskInfo"} component={TaskInfo} />
-    // </Stack.Navigator>
-    <ToDoListStack toDo={toDo} navigation={navigation} />
-  );
+  return <ToDoListStack toDo={toDo} navigation={navigation} />;
 }
