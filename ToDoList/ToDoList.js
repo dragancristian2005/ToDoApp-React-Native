9c8c37f0dedb9ab79e5653c57d12fef2733457ef
Cristian Dragan <cristiandragan34@yahoo.ro>
2024-11-12 19:54:40 +0200

    test commit

--- ToDoList/ToDoList.js
+++ ToDoList/ToDoList.js
@@ -1,4 +1,3 @@
-import { createStackNavigator } from "@react-navigation/native/src/__stubs__/createStackNavigator";
 import ToDoListStack from "../src/components/ToDoListStack";
 import * as React from "react";
 
